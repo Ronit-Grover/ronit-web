93a2bc718eb1be63c2e83cb4b881b280804eee71
Ronit Grover <groverronit99@gmail.com>
2023-11-23 00:12:17 +0530

    Update getter.js

--- utils/getter.js
+++ utils/getter.js
@@ -140,7 +140,7 @@ export const getHelp = () => {
 }
 
 export const redirectResume = () => {
-  window.open("https://drive.google.com/file/d/19RKJX56TsFcxFVAjSGNi8slyD4T8nDnM/view?usp=sharing", "_blank")
+  window.open("https://drive.google.com/file/d/1kqjauzugsueni5jfo-Chu1MAyoVrMf6Z/view?usp=sharing", "_blank")
   return ""
 }
 
